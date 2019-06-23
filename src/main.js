c3b944055425e5674320100fcd1905cc36c16cbb
Julien de Castelnau <tripod320@gmail.com>
2019-06-22 23:13:09 -0500

    First commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
+import './plugins/vuetify'
 import App from './App.vue';
 import router from './router';
+import { firestorePlugin } from 'vuefire'
+
+Vue.use(firestorePlugin)
 
 Vue.config.productionTip = false;
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Login from './views/Login.vue';
+import Camera from './views/Camera.vue';
 
 Vue.use(Router);
 
@@ -21,5 +23,15 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/camera',
+      name: 'camera',
+      component: Camera,
+    }
   ],
 });
